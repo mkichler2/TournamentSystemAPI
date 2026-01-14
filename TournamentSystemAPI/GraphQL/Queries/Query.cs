0c1737897b07ccc4666eb7139fe615038c054736
Maciej Kichler <MACIEJ.KICHLER@gmail.com>
2026-01-14 19:19:22 +0100

    Testyv2

--- TournamentSystemAPI/GraphQL/Queries/Query.cs
+++ TournamentSystemAPI/GraphQL/Queries/Query.cs
@@ -3,15 +3,21 @@ using HotChocolate;
 using HotChocolate.AspNetCore.Authorization;
 using TournamentSystemAPI.Data;
 using TournamentSystemAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace TournamentSystemAPI.GraphQL.Queries
 {
     public class Query
     {
         // Publiczny dostęp do turniejów
-        public IQueryable<Tournament> GetTournaments([Service] AppDbContext context) => context.Tournaments;
+        public IQueryable<Tournament> GetTournaments([Service] AppDbContext context) 
+    => context.Tournaments
+        .Include(t => t.Bracket)
+            .ThenInclude(b => b.Matches)
+                .ThenInclude(m => m.Winner)
+        .Include(t => t.Participants);
 
-        // Wymaganie: "Użytkownik po zalogowaniu się, ma możliwość pobrania informacji o swoich meczach"
+        // Użytkownik po zalogowaniu się ma możliwość pobrania informacji o swoich meczach"
         [Authorize]
         public IQueryable<Match> GetMyMatches([Service] AppDbContext context, [Service] IHttpContextAccessor httpContextAccessor)
         {
